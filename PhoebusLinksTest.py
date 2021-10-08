981a41badabc5cae9514eb3c51305ee4257c536e
Lucas Lins <lucas11lins@gmail.com>
2021-10-08 13:52:32 -0300

    Retornado para versão 1.1

--- PhoebusLinksTest.py
+++ PhoebusLinksTest.py
@@ -59,6 +59,34 @@ image_3 = canvas.create_image(
     image=image_image_3
 )
 
+image_image_4 = ImageTk.PhotoImage(relative_to_assets("image_4.png"))
+image_4 = canvas.create_image(
+    285.99999999999994,
+    347.0,
+    image=image_image_4
+)
+
+image_image_5 = ImageTk.PhotoImage(relative_to_assets("image_5.png"))
+image_5 = canvas.create_image(
+    205.99999999999994,
+    473.0,
+    image=image_image_5
+)
+
+image_image_6 = ImageTk.PhotoImage(relative_to_assets("image_6.png"))
+image_6 = canvas.create_image(
+    118.99999999999994,
+    480.0,
+    image=image_image_6
+)
+
+image_image_7 = ImageTk.PhotoImage(relative_to_assets("image_7.png"))
+image_7 = canvas.create_image(
+    288.99999999999994,
+    480.0,
+    image=image_image_7
+)
+
 button_image_1 = ImageTk.PhotoImage(relative_to_assets("button_1.png"))
 button_1 = Button(
     image=button_image_1,
@@ -119,11 +147,86 @@ button_4.place(
     height=44.0
 )
 
+button_image_5 = ImageTk.PhotoImage(relative_to_assets("button_5.png"))
+button_5 = Button(
+    image=button_image_5,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: bsC(),
+    relief="flat"
+)
+button_5.place(
+    x=36.99999999999994,
+    y=337.0,
+    width=178.0,
+    height=44.0
+)
+
+button_image_6 = ImageTk.PhotoImage(relative_to_assets("button_6.png"))
+button_6 = Button(
+    image=button_image_6,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: ocS(),
+    relief="flat"
+)
+button_6.place(
+    x=218.99999999999994,
+    y=337.0,
+    width=179.0,
+    height=44.0
+)
+
+button_image_7 = ImageTk.PhotoImage(relative_to_assets("button_7.png"))
+button_7 = Button(
+    image=button_image_7,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: cpA(),
+    relief="flat"
+)
+button_7.place(
+    x=400.99999999999994,
+    y=337.0,
+    width=134.0,
+    height=44.0
+)
+
+button_image_8 = ImageTk.PhotoImage(relative_to_assets("button_8.png"))
+button_8 = Button(
+    image=button_image_8,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: gSkins(),
+    relief="flat"
+)
+button_8.place(
+    x=47.99999999999994,
+    y=470.0,
+    width=134.0,
+    height=44.0
+)
+
+button_image_9 = ImageTk.PhotoImage(relative_to_assets("button_9.png"))
+button_9 = Button(
+    image=button_image_9,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: kVersions(),
+    relief="flat"
+)
+button_9.place(
+    x=217.99999999999994,
+    y=470.0,
+    width=134.0,
+    height=44.0
+)
+
 canvas.create_text(
     430.0,
     10.0,
     anchor="nw",
-    text="Versão: 1.1 - 29/09/21",
+    text="Versão: 1.2 - 08/10/21",
     fill="#828282",
     font=("Poppins Medium", 12 * -1)
 )
